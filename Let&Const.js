2d568b6c5515b4165d63a4c136ac4c8114ca9b44
Saadman <saadmansakib34@gmail.com>
2025-08-02 12:45:39 +0600

    Added

---
+++ Let&Const.js
@@ -0,0 +1,17 @@
+//var vs let vs const
+
+
+// var fullName = 'Saadman Sakib';
+// var fullName = 'JavaScript'
+// console.log(fullName);
+
+
+// let fullName = 'Saadman Sakib';
+// let fullName2 = 'JS';
+// console.log(fullName);
+
+
+//const is locked and not change-able
+
+const fullName = 'Saadman Sakib';
+console.log(fullName);
---
+++ NumberMethod.js
@@ -0,0 +1,20 @@
+// var num = 20;
+
+// num = toString(num);
+
+
+// var num = "20.5";
+// num = parseInt(num);
+
+//console.log(typeof(num));
+
+//var number = 2.5678;
+
+// console.log(number.toFixed(2));
+
+// console.log(number.toPrecision(4));
+
+console.log(typeof(Number("12")))
+
+
+console.log(Number("    12.5   "))
